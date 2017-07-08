775b5dfeab753f8dce9a1ddb0bff348de8873581
Janes T <Janes T>
2017-07-09 01:09:41 +0530

    Removed www

--- www/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Person
-# Create your views here.
-def home(request):
-    people = Person.objects.all()
-    return render(request,'home.html',{'people':people})
